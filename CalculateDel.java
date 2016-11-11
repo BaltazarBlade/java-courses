0571c2c9386f4e06a71b827799b72eb07f3c7b04
Дима <>
2016-11-11 13:15:05 +0200

    create CalculateDel

---
+++ CalculateDel.java
@@ -0,0 +1,9 @@
+public class CalculateDel {
+	public static void main (String[] arg) {
+		System.out.println("Calculate...");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int del = first / second;
+		System.out.println("Del= " + del);
+	}
+}
